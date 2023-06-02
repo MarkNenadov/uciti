e232d41951b00eeb51601db486852918cc0eb0e7
Mark Nenadov <marknenadov@gmail.com>
2023-06-02 13:34:11 -0400

    fix start's css watch and some other small things

--- src/Components/FlashCardControlBar.tsx
+++ src/Components/FlashCardControlBar.tsx
@@ -22,7 +22,7 @@ export function FlashCardControlBar( {startingLanguage, setStartingLanguage, set
                <div 
                     aria-label={"Shuffle Cards"}
                      onClick={ () => { shuffleCards( cardsPerPage ) } } 
-                    className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center lg:text-left"
+                    className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center lg:text-left w-full"
                 >
                     Shuffle Cards
             </div>

59051d0cd7e16942d351cd51174d2a31a6f6eea9
Mark Nenadov <marknenadov@gmail.com>
2023-09-06 18:47:39 -0400

    better  RandomUtils implementation & dep updates

--- src/Utils/RandomUtils.ts
+++ src/Utils/RandomUtils.ts
@@ -1,21 +1,13 @@
 export function pickRandomItems<T>(array: T[], count: number): T[] {
-  const shuffled = array.slice();
-  let currentIndex = shuffled.length;
-  const result: T[] = [];
-
-  while (count > 0 && currentIndex > 0) {
-    const randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    [shuffled[currentIndex], shuffled[randomIndex]] = [
-      shuffled[randomIndex],
-      shuffled[currentIndex],
-    ];
-    result.push(shuffled[currentIndex]);
-    count--;
+  if (count <= 0) {
+    throw new Error("Count must be greater than 0");
+  }
+  if (count > array.length) {
+    throw new Error("Count must not exceed the length of the array");
   }
 
-  return result;
+  const shuffled = shuffleArray(array.slice());
+  return shuffled.slice(0, count);
 }
 
 export function shuffleArray<T>(array: T[]): T[] {
